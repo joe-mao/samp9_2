bc847b6b9fe004db64f946a3c66b87b1ac2e7a9b
joe-mao <44386522+joe-mao@users.noreply.github.com>
2019-06-19 22:06:23 +0800

    Add files via upload

--- qwdialogpen.cpp
+++ qwdialogpen.cpp
@@ -5,7 +5,23 @@ void QWDialogPen::setPen(QPen pen)
 {
     //设置QPen,并刷新显示界面
     m_pen= pen;
+    this->ui->spinWidth->setValue(pen.width());//线宽
+    int i = static_cast<int>(pen.style());//枚举类型转换为整型
+    this->ui->comboPenStyle->setCurrentIndex(i);
+    QColor color = pen.color();
+    this->ui->btnColor->setAutoFillBackground(true);//设置颜色按钮的背景色
+    QString str = QString::asprintf("background-color: rgb(%d, %d, %d);", color.red(), color.green(), color.blue());
+    this->ui->btnColor->setStyleSheet(str);
+}
 
+QPen QWDialogPen::getPen()
+{
+    //获得设置的属性
+    m_pen.setStyle(Qt::PenStyle(this->ui->comboPenStyle->currentIndex()));//线型
+    m_pen.setWidth(this->ui->spinWidth->value());//线宽
+    QColor color = this->ui->btnColor->palette().color(QPalette::Button);
+    m_pen.setColor(color);//颜色
+    return m_pen;
 }
 
 QPen QWDialogPen::getPen(QPen iniPen, bool &ok)
@@ -26,10 +42,7 @@ QPen QWDialogPen::getPen(QPen iniPen, bool &ok)
     return pen;//返回设置的QPen对象
 }
 
-void QWDialogPen::setPen(QPen pen)
-{
-    //设置QPen,并刷新显示界面
-}
+
 
 
 QWDialogPen::QWDialogPen(QWidget *parent) :
